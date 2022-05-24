63aac1f1f00e23510cf6e2b3f6eab74775105f96
David Alliger <alligerdavidj@gmail.com>
2022-05-24 14:06:28 -0500

    Removed commented out code and console logs from accounts routes.

--- backend/routes/api/accounts.js
+++ backend/routes/api/accounts.js
@@ -15,10 +15,6 @@ router.get('/', asyncHandler(async(req, res) => {
             accounts[account.payer] = 0;
         }
     });
-    // const totalPoints = accounts.reduce((sum, account) => {
-    //     sum += account.points;
-    //     return sum;
-    // }, 0);
     return res.json(accounts);
 }));
 
@@ -32,25 +28,6 @@ router.get('/total', asyncHandler(async(req, res) => {
     return res.json(totalPoints);
 }));
 
-// router.post('/', asyncHandler(async(req, res) => {
-//     const {
-//         payer,
-//         points
-//     } = req.body;
-
-//     const account = await Account.create({
-//         payer,
-//         points
-//     });
-
-//     const response = {
-//         payer: account.payer,
-//         points: account.points
-//     };
-
-//     return res.json(response);
-// }));
-
 router.put('/', asyncHandler(async(req, res) => {
     let { points } = req.body;
 
@@ -58,7 +35,6 @@ router.put('/', asyncHandler(async(req, res) => {
         include: [Account],
         order: [['timestamp','ASC']]
     });
-    // console.log(transactions);
 
     let round = 1;
     let count = 1;
@@ -163,80 +139,5 @@ router.put('/', asyncHandler(async(req, res) => {
 
     return res.json({response: response});
 }));
-
-
-// router.put('/', asyncHandler(async(req, res) => {
-//     let { points } = req.body;
-
-//     const transactions = await Transaction.findAll({
-//         order: [['timestamp','ASC']]
-//     });
-//     console.log(transactions);
-
-//     const response = [];
-
-//     transactions.forEach(async(transaction) => {
-//         const account = await Account.findOne({
-//             where: {
-//                 id: transaction.accountId
-//             }
-//         });
-//         if (transaction.remaining > 0) {
-//             if (transaction.remaining >= points) {
-//                 await Transaction.update({
-//                     remaining: transaction.remaining - points
-//                 },
-//                 { where: {
-//                     id: transaction.id
-//                 }});
-//                 const newTransaction = await Transaction.create({
-//                     accountId: transaction.accountId,
-//                     points: -points,
-//                     remaining: 0,
-//                     timestamp: Date.now()
-//                 });
-//                 response.push({
-//                     payer: account.payer,
-//                     points: newTransaction.points
-//                 });
-//                 await Account.update({
-//                     points: account.points - points
-//                 },
-//                 {
-//                     where: {
-//                         id: account.id
-//                     }
-//                 });
-//             } else {
-//                 await Account.update({
-//                     points: account.points - transaction.remaining
-//                 },
-//                 {
-//                     where: {
-//                         id: account.id
-//                     }
-//                 });
-//                 const newTransaction = await Transaction.create({
-//                     accountId: transaction.accountId,
-//                     points: -transaction.remaining,
-//                     remaining: 0,
-//                     timestamp: Date.now()
-//                 });
-//                 response.push({
-//                     payer: account.payer,
-//                     points: newTransaction.points
-//                 });
-//                 await Transaction.update({
-//                     remaining: 0
-//                 },
-//                 { where: {
-//                     id: transaction.id
-//                 }});
-//             }
-//         }
-//     })
-
-//     return res.json(response);
-// }));
 
 module.exports = router;
