24952d9043080dcf51d8b72422967374100f5f0b
David Alliger <alligerdavidj@gmail.com>
2022-05-25 12:39:34 -0500

    Updated routes to serve index.html in production.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,5 +6,22 @@ const router = express.Router();
 
 router.use('/api', apiRouter);
 
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
+
+    router.get('/', (req, res) => {
+        return res.sendFile(
+            path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+        );
+    });
+
+    router.use(express.static(path.resolve('../frontend/build')));
+
+    router.get(/^(?!\/?api).*/, (req, res) => {
+        return res.sendFile(
+            path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+        );
+    });
+}
 
 module.exports = router;
